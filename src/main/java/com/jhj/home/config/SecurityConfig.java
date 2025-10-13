b824cda8880a051eddb99c0c18a7b76c52cd9ee7
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-13 15:26:25 +0900

    보안 관련

--- src/main/java/com/jhj/home/config/SecurityConfig.java
+++ src/main/java/com/jhj/home/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -75,4 +76,9 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
+    
+   
+
 }
+
+
