beef38635f8849d460d205503f6bc95ac56e24b4
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-13 15:51:23 +0900

    로그인

--- src/main/java/com/jhj/home/config/SecurityConfig.java
+++ src/main/java/com/jhj/home/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
