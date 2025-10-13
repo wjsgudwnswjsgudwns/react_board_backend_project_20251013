966c7b25d7c60be209884de05fffff004ad5d232
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-13 11:15:25 +0900

    시작

---
+++ src/main/java/com/jhj/home/ReactBoardBackendProject20251013Application.java
@@ -0,0 +1,13 @@
+package com.jhj.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBoardBackendProject20251013Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBoardBackendProject20251013Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhj/home/ReactBoardBackendProject20251013ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhj.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBoardBackendProject20251013ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
