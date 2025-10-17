86bb1647333c16d9f931e5be07bb0f2168163c40
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-17 10:54:29 +0900

    cascade 설정

--- src/main/java/com/jhj/home/entity/Board.java
+++ src/main/java/com/jhj/home/entity/Board.java
@@ -1,14 +1,18 @@
 package com.jhj.home.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,4 +34,7 @@ public class Board {
 	
 	@ManyToOne
 	private SiteUser author; // 글쓴이
+	
+	@OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Comment> comments = new ArrayList<>();
 }
